d6c51043760a641cd89b8865b74b7a5367a27f59
Jay <jaysebagh@gmail.com>
2022-10-30 11:40:10 -0700

    created a basic routing with a Home, About, and Contact page

--- route/src/App.js
+++ route/src/App.js
@@ -1,7 +1,23 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+const Home = () => { return <h1>Home Component</h1> },
+    About = () => { return <h1>About Component</h1> },
+    Contact = () => { return <h1>Contact Component</h1> }
+
 function App() {
   return (
     <div>
-      hello
+        <header>
+            <Link to = '/'>Home </Link>
+            <Link to = '/about'>About </Link>
+            <Link to = '/contact'>Contact</Link>
+
+            <Routes>
+                <Route exact path = "/" element = {Home()} />
+                <Route path = "/contact" element = {Contact()} />
+                <Route path = "/about" element = {About()} />
+            </Routes>
+        </header>
     </div>
   );
 }
